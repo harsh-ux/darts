d1db03b51e15e272f23df90d0c0ff30b1c282f0e
Harsh Agarwal <agarwal.10@iitj.ac.in>
2022-04-28 17:20:22 +0530

    changes done

--- cnn/utils.py
+++ cnn/utils.py
@@ -28,11 +28,11 @@ def accuracy(output, target, topk=(1,)):
 
   _, pred = output.topk(maxk, 1, True, True)
   pred = pred.t()
-  correct = pred.eq(target.view(1, -1).expand_as(pred))
+  correct = pred.eq(target.reshape(1, -1).expand_as(pred))    ##change done
 
   res = []
   for k in topk:
-    correct_k = correct[:k].view(-1).float().sum(0)
+    correct_k = correct[:k].view(-1).float().sum(0)    ##change may be required view->require
     res.append(correct_k.mul_(100.0/batch_size))
   return res
 
